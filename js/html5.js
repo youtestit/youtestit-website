4bae47d7ec91efb57a7e6144be38d9fa076c187b
Patrick Guillerm <patrickguillerm@gmail.com>
2012-04-18 23:04:21 +0200

    append seam rest project developement in progress...

--- js/html5.js
+++
@@ -1,8 +0,0 @@
-document.createElement("header"); 
-document.createElement("footer"); 
-document.createElement("section"); 
-document.createElement("aside"); 
-document.createElement("nav");
-document.createElement("menu"); 
-document.createElement("article"); 
-document.createElement("figure"); 
